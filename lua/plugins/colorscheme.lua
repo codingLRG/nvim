1f91c9768f3f7240b58cf4f02120dc1b9c747495
codingLRG <flinglion@gmail.com>
2025-07-12 13:05:30 -0700

    treesitter and theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,1 +1,11 @@
-return{}
+return{
+{
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+
+    config = function()
+	vim.cmd("colorscheme cyberdream")
+	end,
+}
+}
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,16 @@
 return{
-	{"nvim-treesitter/nvim-treesitter",
-	branch = 'master', 
-	lazy = false, 
+	{
+	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	config = function()
+		require'nvim-treesitter.configs'.setup {
+  		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "gdscript" },
+		auto_install = true,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+			},
+		}
+	end,
 	}
 }
