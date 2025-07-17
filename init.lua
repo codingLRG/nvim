e5915e3d2d3b2569af7c8c5840a0a779ab440ffd
CodingLRG <flinglion@gmail.com>
2025-07-17 16:36:29 -0700

    Adjusted plugins

--- init.lua
+++ init.lua
@@ -1,27 +1,37 @@
-require("config.lazy")
+require 'config.options'
+require 'config.keymaps'
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
 
---- .vim settings ---
-local o = vim.opt
--- Sidebar adjustment
-o.cursorline = true
-o.number = true
-o.relativenumber = true
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
--- Color/Search
-o.syntax = "on"
-o.hlsearch = true
-o.incsearch = true
-o.ignorecase = true
+-- Setup lazy.nvim
+require("lazy").setup({
+   require 'plugins.colorscheme',
+   require 'plugins.treesitter',
+   require 'plugins.neo-tree',
+   require 'plugins.bufferline',
+   require 'plugins.statusline',
+   require 'plugins.tab-assist',
+   require 'plugins.startup-page',
 
--- File Formatting
-o.compatible = false
-o.filetype = "on"
-o.swapfile = false
-o.autoindent = true
-
--- Clipboard
-o.clipboard="unnamedplus"
-
--- Spelling
-o.spell = true
+})
